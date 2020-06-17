05fb978add8dd2a021167d91e9782fcf1bc404e1
master <62730023+Dionatas1114@users.noreply.github.com>
2020-06-16 23:59:34 -0300

    no message

---
+++ index.js
@@ -0,0 +1,15 @@
+Faça uma aplicação back - end para cadastrar usuários:
+    você terá uma lista vazia e recebera os seguintes dados de uma rota:
+
+    Email
+Nome
+Idade
+Telefone
+
+Em seguida adicione o objeto com esses dados a lista.
+Você também deve fazer uma rota para visualizar todos os usuários cadastrados.
+
+DESAFIO DENTRO DO DESAFIO😆
+O desafio é atualizar um dado de um usuário já cadastrado.
+
+*Obs: deve ser entregue via github.
